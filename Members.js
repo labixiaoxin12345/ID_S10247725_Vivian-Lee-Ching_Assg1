d0e87fd521b6aa9364fbad49c167cd1c590078f2
labixiaoxin12345 <vivianleeching@hotmail.com>
2022-11-20 16:00:17 +0800

    commit2

---
+++ Members.js
@@ -0,0 +1,36 @@
+class Members{
+    constructor(
+    //parameters
+    nameAdam,
+    ageAdam,
+    name2,
+    age2,
+    name3,
+    age3,
+    name4,
+    age4
+    
+    ) {
+    //properties
+    this.member1 = {
+        nameAdam: nameAdam,
+        ageAdam: ageAdam,
+    }
+    this.member2 = {
+        name2 : name2,
+        age2: age2 
+    }
+    this.member3 = {
+        name3 : name3,
+        age3 : age3
+    }
+    this.member4 = {
+        name4 : name4,
+        age4 : age4
+    }
+    }
+    //methods
+
+}
+
+export default Members;                                     
---
+++ index.js
@@ -0,0 +1,18 @@
+// Create a class for the Member object type.
+
+ 
+ import Member from "./Members.js";
+
+ const Member = new Member(
+    "Adam Levine",
+    "xx",
+    "name 2",
+    "xx",
+    "name 3",
+    "xx",
+    "name 4",
+    "xx",
+ );
+ 
+
+ 
