896dedbf6a1c1ee7053422791a55be2148f29c5e
labixiaoxin12345 <vivianleeching@hotmail.com>
2022-11-27 21:19:55 +0800

    updated img, changed css styling

--- index.js
+++ index.js
@@ -3,11 +3,11 @@
  
  import Member from "./Members.js";
 
- const adam = new Member("Adam Levine","43");
+ const adam = new Member("Adam Levine", "43");
 
  const james = new Member("James Burgon Valentine", "44");
 
- const pj = new Member("PJ Morton","41");
+ const pj = new Member("PJ Morton", "41");
 
  const jesse = new Member("Jesse Carmichael", "43");
 
@@ -18,20 +18,22 @@
 
  console.log(adam.age, james.age, pj.age, jesse.age, matt.age, sam.age);
 
- //create a new element using javascript
+ 
+//create a new element using javascript
 let newDiv1 = document.createElement("div");
 //add class to element through javascript
 newDiv1.classList.add("mystyle")
 // and give it some content
-newDiv1.innerHTML = "Adam: <br>" +this.name;
+//newDiv1.innerHTML = "add <br>" +Member.adam;
+
 //add the div to the body of the html
 document.body.appendChild(newDiv1);
-
+/*
 //create a new element using javascript
 let newDiv2 = document.createElement("div");
 //add class to element through javascript
 newDiv2.classList.add("mystyle")
 // and give it some content
-newDiv2.innerHTML = "Minabae's Singer Profile: <br>" +minabae.singerProfile()+minabae.showHitpointsStatus();
+newDiv2.innerHTML = "Minabae's Singer Profile: <br>" +minabae.singerProfile();
 //add the div to the body of the html
-document.body.appendChild(newDiv2);
+document.body.appendChild(newDiv2);*/
